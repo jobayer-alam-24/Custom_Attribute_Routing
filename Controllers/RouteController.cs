1e36832ea514b4b477fb65ce2fa76dd0023ff502
jobayer-alam-24 <sheikhnayamul123@gmail.com>
2024-10-13 15:07:47 +0600

    working

---
+++ Controllers/RouteController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Custom_Attribute_Routing.Controllers
+{
+    public class RouteController : Controller
+    {
+        private readonly ILogger<RouteController> _logger;
+
+        public RouteController(ILogger<RouteController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Custom_Attribute_Routing.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
